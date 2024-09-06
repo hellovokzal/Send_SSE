dd6aab8c370e5be5296bdcb8bb7a419e2e53dda4
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2024-09-06 19:57:59 +0500

    Create send_sse.py

---
+++ send_sse.py
@@ -0,0 +1,40 @@
+try:
+  from flask import Flask, Response, render_template
+  import time
+  import os
+except:
+  from os import *
+  system("pip install flask")
+  from flask import Flask, Response, render_template
+  import time
+# Создаем директорию hhh, если ее не существует
+if not os.path.exists("hhh"):
+    os.mkdir("hhh")
+
+app = Flask(__name__)
+
+def generate_events():
+    time.sleep(0.05)
+    num = 0
+    """Генерация событий в реальном времени."""
+    while True:
+        time.sleep(0.05)
+        num += 1
+        yield f"data: {num}\n\n"
+
+@app.route('/')
+def index():
+    """Главная страница приложения."""
+    try:
+        with open("index.html", "r") as fl:
+            return str(fl.read())
+    except FileNotFoundError:
+        return "Ошибка: файл index.html не найден.", 404
+
+@app.route('/stream')
+def stream():
+    """Обработчик для события stream."""
+    return Response(generate_events(), content_type='text/event-stream')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
